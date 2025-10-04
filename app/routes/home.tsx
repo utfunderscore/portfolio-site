d83b39331b86deb16f6d23743b56c0c12b2838db
utfunderscore <83981186+utfunderscore@users.noreply.github.com>
2025-10-04 22:26:52 +0100

    feat: Add home and CLI routes with corresponding styles

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,5 +1,6 @@
 import { useState, useEffect } from "react";
 import { useLoaderData } from "react-router";
+import "./home.css";
 import type { Route } from "./+types/home";
 import type { Post } from "~/types";
 import { BlogPosts } from "~/components/blog-posts";
