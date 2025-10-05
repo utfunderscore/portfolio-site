793610dcc498b5945ac1758f6c9189c8cfd546d1
utfunderscore <83981186+utfunderscore@users.noreply.github.com>
2025-10-05 01:13:00 +0100

    feature: Start building out cli style page

--- app/components/contact-card.tsx
+++ app/components/contact-card.tsx
@@ -1,4 +1,3 @@
-import type { CSSProperties } from "react";
 
 export function ContactCard() {
   return (
--- app/routes.ts
+++ app/routes.ts
@@ -2,5 +2,5 @@ import { type RouteConfig, index, route } from "@react-router/dev/routes";
 
 export default [
   index("routes/home.tsx"),
-  route("cli", "routes/cli.tsx")
+  route("cli", "routes/cli/cli.tsx")
 ] satisfies RouteConfig;
